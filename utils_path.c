2b66ef5351401af3c5a1fdbc7d8b3912c19c8dc5
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2022-12-03 01:24:00 -0500

    Update utils_path.c

--- utils_path.c
+++ utils_path.c
@@ -386,42 +386,18 @@ void sphereIntersect(struct object3D *sphere, struct ray3D *ray, double *lambda,
 
         // normal mapping
         if (sphere->normalMap) {
-            double bump_R, bump_G, bump_B;
-            sphere->textureMap(sphere->normalMap, *a, *b, &bump_R, &bump_G, &bump_B);
+            rgb_to_coord(n, sphere, *a, *b);
 
-            struct point3D normalTex;
-            normalTex.px = 2 * bump_R - 1;
-            normalTex.py = 2 * bump_G - 1;
-            normalTex.pz = 2 * bump_B - 1;
-            struct point3D tangent;
-            tangent.px = canonical_normal.py;
-            tangent.py = -canonical_normal.px;
-            tangent.pz = 0;
-            struct point3D *binormal = cross(&tangent, &canonical_normal);
-
-            double M[4][4] = {
-                    {canonical_normal.py,  binormal->px, canonical_normal.px, 0.0},
-                    {-canonical_normal.px, binormal->py, canonical_normal.py, 0.0},
-                    {canonical_normal.pz,  binormal->pz, canonical_normal.pz, 0.0},
-                    {0.0,          0.0,          0.0,         1.0}};
-
-            matVecMult(M, &normalTex);
-            normalize(&normalTex);
-
-            canonical_normal.px = normalTex.px;
-            canonical_normal.py = normalTex.py;
-            canonical_normal.pz = normalTex.pz;
-
-            free(binormal);
+            struct point3D *tangent = newPoint(p->py, -p->px, 0);
+            tbn_transform(&canonical_normal, tangent, n);
+            free(tangent);
         }
-
-        // get the actual intersection point
-        (ray_trans.rayPos)(ray, *lambda, p);
-
-        // get the actual normal
-        normalTransform(&canonical_normal, n, sphere);
-
     }
+    // get the actual intersection point
+    (ray_trans.rayPos)(ray, *lambda, p);
+
+    // get the actual normal
+    normalTransform(&canonical_normal, n, sphere);
 }
 
 void cylIntersect(struct object3D *cylinder, struct ray3D *r, double *lambda, struct point3D *p, struct point3D *n,
